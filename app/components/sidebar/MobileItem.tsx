d384ba694bf5f108fd51a9d44750b4968ac87283
rod_cris2 <rodcris70@gmail.com>
2023-08-24 16:22:20 -0700

    final

---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import clsx from "clsx";
+
+interface MobileItemsProps {
+    href: string;
+    icon: any;
+    active?: boolean;
+    onClick?: () => void;
+}
+
+const MobileItem: React.FC<MobileItemsProps> = ({
+    href,
+    icon: Icon,
+    active,
+    onClick
+}) => {
+    const handleClick = () => {
+        if(onClick) {
+            return onClick();
+        }
+    }
+
+  return (
+    <Link onClick={onClick} href={href} className={clsx(`group flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-509 hover:text-black hover:bg-gray-100`, active && "bg-gray-100 text-black")}>
+        <Icon className="h-6 w-5" />
+    </Link>
+  )
+}
+
+export default MobileItem;
